2ce4bea35f3c2d161e08742a2381c0bff89149e9
Drew Carey Buglione <me@drewb.ug>
2014-05-27 01:17:52 -0400

    MainActivity extends ListActivity

--- src/ug/drewb/rawnfc/MainActivity.java
+++ src/ug/drewb/rawnfc/MainActivity.java
@@ -1,15 +1,14 @@
 package ug.drewb.rawnfc;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
+public class MainActivity extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
     }
 }
