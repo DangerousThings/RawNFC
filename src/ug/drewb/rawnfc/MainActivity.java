b426c1cb8c1ed92a68f5899909cfe6bdcf76df75
A. C. Buglione <drew@dangerousthings.com>
2014-05-27 09:34:31 -0400

    MainActivity#onCreate

--- src/ug/drewb/rawnfc/MainActivity.java
+++ src/ug/drewb/rawnfc/MainActivity.java
@@ -2,6 +2,7 @@ package ug.drewb.rawnfc;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity
 {
@@ -10,5 +11,9 @@ public class MainActivity extends ListActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        final String[] values = new String[] {"NFC-A (ISO 14443-3A)", "NFC-B (ISO 14443-3B)", "NFC-F (JIS 6319-4)", "NFC-V (ISO 15693)", "ISO-DEP (ISO 14443-4)"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 }
