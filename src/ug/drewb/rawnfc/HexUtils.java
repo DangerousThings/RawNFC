994e5f945c5cfc401197234ba6913d5b81880713
A. C. Buglione <drew@dangerousthings.com>
2014-05-30 17:50:45 -0400

    TagTechnology

---
+++ src/ug/drewb/rawnfc/HexUtils.java
@@ -0,0 +1,27 @@
+package ug.drewb.rawnfc;
+
+public class HexUtils
+{
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars).replaceAll("..(?!$)", "$0 ");
+    }
+
+    public static byte[] hexToBytes(String hex) {
+        hex = hex.replaceAll("\\s+","");
+        int len = hex.length();
+        byte[] bytes = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                                  + Character.digit(hex.charAt(i+1), 16));
+        }
+        return bytes;
+    }
+}
