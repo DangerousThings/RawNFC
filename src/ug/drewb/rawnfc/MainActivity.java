76721a7f9fb5448f0f67db8afb1bbb220710c350
A. C. Buglione <drew@dangerousthings.com>
2014-05-30 17:53:46 -0400

    NfcAActivity

--- src/ug/drewb/rawnfc/MainActivity.java
+++ src/ug/drewb/rawnfc/MainActivity.java
@@ -1,8 +1,11 @@
 package ug.drewb.rawnfc;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ListActivity
 {
@@ -15,5 +18,12 @@ public class MainActivity extends ListActivity
         final String[] values = new String[] {"NFC-A (ISO 14443-3A)", "NFC-B (ISO 14443-3B)", "NFC-F (JIS 6319-4)", "NFC-V (ISO 15693)", "ISO-DEP (ISO 14443-4)"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        final Class[] values = new Class[] {NfcAActivity.class};
+        Intent intent = new Intent(this, values[position]);
+        startActivity(intent);
     }
 }
