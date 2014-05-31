415e005bd5a4510dc7705f3668a47cf733e634d7
Drew Carey Buglione <me@drewb.ug>
2014-05-31 17:45:52 -0400

    IsoDepActivity

---
+++ src/ug/drewb/rawnfc/IsoDepActivity.java
@@ -0,0 +1,33 @@
+package ug.drewb.rawnfc;
+
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+
+import java.io.IOException;
+
+public class IsoDepActivity extends TimeoutTagTechnology
+{
+    public void onNewIntent(Intent intent) {
+        IsoDep tag = IsoDep.get((Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG));
+
+        byte[][] commands = getCommands();
+        byte[][] responses = new byte[commands.length][];
+
+        try {
+            tag.connect();
+            if (timeout != 0) {
+                tag.setTimeout(timeout);
+            }
+            for (int i = 0; i < commands.length; i++) {
+                responses[i] = tag.transceive(commands[i]);
+            }
+            tag.close();
+        } catch (IOException e) {
+            showException(e);
+        }
+
+        showResponses(responses);
+    }
+}
